73868547945f699874418cf2988696f70ae29386
Cristiano <cristianoben10@gmail.com>
2022-04-05 02:06:21 +0100

    atualizaçao dashboard e api

--- api/api.php
+++ api/api.php
@@ -4,22 +4,55 @@
 	echo $_SERVER['REQUEST_METHOD']."<br>";
 	if($_SERVER['REQUEST_METHOD'] == 'POST')
 	{
-		echo "Recebi um POST <br>";
-		$a = array($_POST);
-		print_r($_POST);
-		//$a = array($_POST);
-		file_put_contents("files/".$_POST['nome']."/valor.txt",$_POST['valor']);
-		file_put_contents("files/".$_POST['nome']."/hora.txt",$_POST['hora']);
-		file_put_contents("files/".$_POST['nome']."/nome.txt",$_POST['nome']);
-		file_put_contents("files/".$_POST['nome']."/log.txt",$_POST['hora']);
-
+		if(isset($_POST['valor']) && isset($_POST['hora']) && isset($_POST['nome']) && isset($_POST['hora']))
+		{
+			file_put_contents("files/".$_POST['nome']."/valor.txt",$_POST['valor']);
+			file_put_contents("files/".$_POST['nome']."/hora.txt",$_POST['hora']);
+			file_put_contents("files/".$_POST['nome']."/nome.txt",$_POST['nome']);
+			file_put_contents("files/".$_POST['nome']."/log.txt",$_POST['hora']);
+		}
+		if(isset($_POST['valor']) && isset($_POST['hora']) && isset($_POST['nome']) && isset($_POST['hora']))
+		{
+			file_put_contents("files/".$_POST['nome']."/valor.txt",$_POST['valor']);
+			file_put_contents("files/".$_POST['nome']."/hora.txt",$_POST['hora']);
+			file_put_contents("files/".$_POST['nome']."/nome.txt",$_POST['nome']);
+			file_put_contents("files/".$_POST['nome']."/log.txt",$_POST['hora']);
+		}
+		if(isset($_POST['valor']) && isset($_POST['hora']) && isset($_POST['nome']) && isset($_POST['hora']))
+		{
+			file_put_contents("files/".$_POST['nome']."/valor.txt",$_POST['valor']);
+			file_put_contents("files/".$_POST['nome']."/hora.txt",$_POST['hora']);
+			file_put_contents("files/".$_POST['nome']."/nome.txt",$_POST['nome']);
+			file_put_contents("files/".$_POST['nome']."/log.txt",$_POST['hora']);
+		}
 	}
 	elseif($_SERVER['REQUEST_METHOD'] == 'GET')
 	{
-		echo "Recebi um GET";
+		
+		if(isset($_GET['nome']) && file_exists("files/cozinha/".$_GET['nome']."/valor.txt"))
+		{
+			echo file_get_contents("files/cozinha/".$_GET['nome']."/valor.txt");
+
+			echo "oi";
+		}
+		if(isset($_GET['nome']) && file_exists("files/quarto/".$_GET['nome']."/valor.txt"))
+		{
+			echo file_get_contents("files/quarto/".$_GET['nome']."/valor.txt");
+
+			echo "oiI";
+		}
+		if(isset($_GET['nome']) && file_exists("files/wc/".$_GET['nome']."/valor.txt"))
+		{
+			echo file_get_contents("files/wc/".$_GET['nome']."/valor.txt");
+
+			echo "oiII	";
+		}
+		else
+		{
+			http_response_code(403);
+		}
 	}
 	else
 	{
-		echo "Na tás a fazer nada de jeito";
+		http_response_code(400);
 	}
-  ?>
--- dashboard.php
+++ dashboard.php
@@ -1,28 +1,82 @@
 <?php
-	session_start();
-	if(!isset($_SESSION['user']) && !isset($_SESSION['pass']))
-	{
-		header("Location: http://127.0.0.1/ProjetoIoT/");
-		die("acesso restrito");
-	}
-	/*$valorT = file_get_contents("API/FILES/TEMPERATURA/valor.txt");
-	$horaT = file_get_contents("API/FILES/TEMPERATURA/hora.txt");
-	$nomeT = file_get_contents("API/FILES/TEMPERATURA/nome.txt");*/
+session_start();
+if (!isset($_SESSION['user']) && !isset($_SESSION['pass'])) {
+	header("Location: http://127.0.0.1/ProjetoIoT/");
+	die("acesso restrito");
+}
+
+//Cozinha
+	//Sensor Gás Fumo
+$valorC_GF = file_get_contents("api/files/cozinha/gas_fumo/valor.txt");
+$horaC_GF = file_get_contents("api/files/cozinha/gas_fumo/hora.txt");
+//$nomeC_GF = file_get_contents("api/files/cozinha/gas_fumo/nome.txt");
+
+	//Sensor Movimento
+$valorC_M = file_get_contents("api/files/cozinha/movimento/valor.txt");
+$horaC_M = file_get_contents("api/files/cozinha/movimento/hora.txt");
+//$nomeC_M = file_get_contents("api/files/cozinha/movimento/nome.txt");
+
+
+//Quarto
+	//Sensor Gás Fumo
+$valorQ_GF = file_get_contents("api/files/quarto/gas_fumo/valor.txt");
+$horaQ_GF = file_get_contents("api/files/quarto/gas_fumo/hora.txt");
+//$nomeC_GF = file_get_contents("api/files/cozinha/gas_fumo/nome.txt");
+	
+	//Sensor Luminosidade
+$valorQ_L = file_get_contents("api/files/quarto/luminosidade/valor.txt");
+$horaQ_L = file_get_contents("api/files/quarto/luminosidade/hora.txt");
+//$nomeC_M = file_get_contents("api/files/cozinha/movimento/nome.txt");
+
+	//Motor
+$valorQ_M = file_get_contents("api/files/quarto/motor/valor.txt");
+$horaQ_M = file_get_contents("api/files/quarto/motor/hora.txt");
+//$nomeC_GF = file_get_contents("api/files/cozinha/gas_fumo/nome.txt");
+	
+	//Sensor Movimento
+$valorQ_Mv = file_get_contents("api/files/quarto/movimento/valor.txt");
+$horaQ_Mv = file_get_contents("api/files/quarto/movimento/hora.txt");
+//$nomeC_M = file_get_contents("api/files/cozinha/movimento/nome.txt");
+
+	//Sensor Temperatura
+$valorQ_T = file_get_contents("api/files/quarto/temperatura/valor.txt");
+$horaQ_T = file_get_contents("api/files/quarto/temperatura/hora.txt");
+//$nomeC_GF = file_get_contents("api/files/cozinha/gas_fumo/nome.txt");
+	
+
+//WC
+	//Sensor Gás Fumo
+$valorWC_GF = file_get_contents("api/files/wc/gas_fumo/valor.txt");
+$horaWC_GF = file_get_contents("api/files/wc/gas_fumo/hora.txt");
+//$nomeC_GF = file_get_contents("api/files/cozinha/gas_fumo/nome.txt");
+	
+	//Sensor Movimento
+$valorWC_Mv = file_get_contents("api/files/wc/movimento/valor.txt");
+$horaWC_Mv = file_get_contents("api/files/wc/movimento/hora.txt");
+//$nomeC_M = file_get_contents("api/files/cozinha/movimento/nome.txt");
+
+	//Sensor Temperatura
+$valorWC_T = file_get_contents("api/files/wc/temperatura/valor.txt");
+$horaWC_T = file_get_contents("api/files/wc/temperatura/hora.txt");
+//$nomeC_M = file_get_contents("api/files/cozinha/movimento/nome.txt");
 ?>
+
 <!doctype html>
 <html lang="pt">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="refresh" content="5">
 	<link rel="stylesheet" href="CSS\style.css">
 	<link rel="stylesheet" href="bootstrap\css\bootstrap.min.css">
 	<script src="bootstrap\js\bootstrap.bundle.min.js"></script>
 	<title>Plataforma IoT</title>
-  </head>
-  <body>
-	  <br>
-	
-    <div class='container'>
+</head>
+
+<body>
+	<br>
+
+	<div class='container'>
 		<div class='card'>
 			<div class='card-body'>
 				<img src='Photos/estgLogo.png' class='float-end' alt='ESTG icon' id='estgImg'>
@@ -34,19 +88,17 @@
 	</div>
 	<br>
 
+	<!--Quarto-->
+
 	
-
-
-
-
-
-	<div class="card-header">
-			<b>Quarto</b>
-		</div>
-		<br>
+	<br>
 	<div class="container text-center">
+	<div class="card-header col-sm-12">
+		<b>Quarto</b>
+	</div>
+	<br>
 		<div class="row">
-			<div class="col-sm-4">
+			<div class="col-sm-3">
 				<div class="card">
 					<div class="card-header"><b>Luminusidade: 80%</b></div>
 					<div class="card-body">
@@ -56,9 +108,9 @@
 						Atualização: <a href="">History</a>
 					</div>
 				</div>
-		  </div>
-		  <div class="col-sm-4">
-			<div class="card">
+			</div>
+			<div class="col-sm-3">
+				<div class="card">
 					<div class="card-header"><b>Temperatura: 22º</b></div>
 					<div class="card-body">
 						<img src="Photos/temperature.png" class="card-img-top" alt="sun icon" id="icons">
@@ -67,9 +119,9 @@
 						Atualização: <a href="">History</a>
 					</div>
 				</div>
-		  </div>
-		  <div class="col-sm-4">
-			<div class="card">
+			</div>
+			<div class="col-sm-3">
+				<div class="card">
 					<div class="card-header"><b>Porta: Fechada</b></div>
 					<div class="card-body">
 						<img src="Photos/door.png" class="card-img-top" alt="sun icon" id="icons">
@@ -78,11 +130,8 @@
 						Atualização: <a href="">History</a>
 					</div>
 				</div>
-		  </div>
-		</div>
-		<br>
-		<div class="row">
-			<div class="col-sm-4">
+			</div>
+			<div class="col-sm-3">
 				<div class="card">
 					<div class="card-header"><b>Fumo e Gás: Não detetado </b></div>
 					<div class="card-body">
@@ -92,9 +141,12 @@
 						Atualização: <a href="">History</a>
 					</div>
 				</div>
-		  </div>
-		  <div class="col-sm-4">
-			<div class="card">
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="card">
 					<div class="card-header"><b>Movimento: Não detetado</b></div>
 					<div class="card-body">
 						<img src="Photos/movimento.png" class="card-img-top" alt="sun icon" id="icons">
@@ -103,9 +155,9 @@
 						Atualização: <a href="">History</a>
 					</div>
 				</div>
-		  </div>
-		  <div class="col-sm-4">
-			<div class="card">
+			</div>
+			<div class="col-sm-3">
+				<div class="card">
 					<div class="card-header"><b>Ventoinha: Desativada</b></div>
 					<div class="card-body">
 						<img src="Photos/fan.png" class="card-img-top" alt="sun icon" id="icons">
@@ -114,11 +166,8 @@
 						Atualização: <a href="">History</a>
 					</div>
 				</div>
-		  </div>
-		</div>
-		<br>
-		<div class="row">
-			<div class="col-sm-4">
+			</div>
+			<div class="col-sm-3">
 				<div class="card">
 					<div class="card-header"><b>Janelas: Fechadas </b></div>
 					<div class="card-body">
@@ -128,11 +177,93 @@
 						Atualização: <a href="">History</a>
 					</div>
 				</div>
-		  </div>
+			</div>
 		</div>
 	</div>
+	<br><br>
+
+	<!--Cozinha-->
+
+	
 	<br>
-	<!--
+	<div class="container text-center">
+	<div class="card-header col-sm-12">
+		<b>WC</b>
+	</div>
+	<br>
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="card">
+					<div class="card-header"><b>Fumo e Gás: <?php echo $valorC_GF?></b></div>
+					<div class="card-body">
+						<img src="Photos/fire_gas.png" class="card-img-top" alt="sun icon" id="icons">
+					</div>
+					<div class="card-footer">
+						Atualização: <?php echo $horaC_GF?> <a href="">History</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<div class="card">
+					<div class="card-header"><b>Movimento: <?php echo $valorC_M?> </b></div>
+					<div class="card-body">
+						<img src="Photos/movimento.png" class="card-img-top" alt="sun icon" id="icons">
+					</div>
+					<div class="card-footer">
+						Atualização: <?php echo $horaC_M?> <a href="">History</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br><br>
+
+	<!--WC-->
+
+	
+	<br>
+	<div class="container text-center">
+	<div class="card-header col-sm-12">
+		<b>Cozinha</b>
+	</div>
+	<br>
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="card">
+					<div class="card-header"><b>Fumo e Gás: <?php echo $valorWC_GF?></b></div>
+					<div class="card-body">
+						<img src="Photos/fire_gas.png" class="card-img-top" alt="sun icon" id="icons">
+					</div>
+					<div class="card-footer">
+						Atualização: <?php echo $horaWC_GF?> <a href="">History</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<div class="card">
+					<div class="card-header"><b>Movimento: <?php echo $valorWC_Mv?></b></div>
+					<div class="card-body">
+						<img src="Photos/movimento.png" class="card-img-top" alt="sun icon" id="icons">
+					</div>
+					<div class="card-footer">
+						Atualização: <?php echo $horaWC_Mv?> <a href="">History</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<div class="card">
+					<div class="card-header"><b>Temperatura: <?php echo $valorWC_T?></b></div>
+					<div class="card-body">
+						<img src="Photos/temperature.png" class="card-img-top" alt="sun icon" id="icons">
+					</div>
+					<div class="card-footer">
+						Atualização: <?php echo $horaWC_T?> <a href="">History</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<!--
 	<div class="card">
 		<div class="card-header">
 			<b>Quarto</b>
@@ -265,5 +396,6 @@
 			</div>
 		</div>
 	<br>-->
-  </body>
+</body>
+
 </html>
