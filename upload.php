e3af6f9172686ce96d7016ecf02ffa03f2d2a363
Cristiano <cristianoben10@gmail.com>
2022-06-13 14:44:31 +0100

    Bruh

---
+++ upload.php
@@ -0,0 +1,33 @@
+<?php //tbd
+$target_file = 'Photos/webcam.jpg';
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check file size
+if ($_FILES['imagem']["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") 
+{
+  echo "Sorry, only JPG, JPEG, PNG files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES['imagem']["tmp_name"], $target_file)) 
+  {
+    echo "The file ". htmlspecialchars( basename( $_FILES['imagem']["name"])). " has been uploaded.";
+  } 
+  else 
+  {
+	echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
